dfeddca46a058cece9b1549aa515b0c36a822f64
Sumit Kolekar <94218518+kolekarsumit@users.noreply.github.com>
2023-08-20 11:54:49 +0530

    Update Table_student_ragistration.java

--- Documents/NetBeansProjects/CodezzzAreaa_class/src/codezzzareaa_class/Table_student_ragistration.java
+++ Documents/NetBeansProjects/CodezzzAreaa_class/src/codezzzareaa_class/Table_student_ragistration.java
@@ -18,9 +18,9 @@ public class Table_student_ragistration {
    public static void main(String args[]) {
 	try {
 		Class.forName("oracle.jdbc.driver.OracleDriver");
-		Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","CodeEraa","Code@123");
+		Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","**********","**********");
 		Statement smt=con.createStatement();
-		smt.executeUpdate("create table New_Coder(name varchar(20),age number,gender varchar(20),phno number(20),code_lang varchar(20),uname varchar(20),pass varchar(20))");
+		smt.executeUpdate("create table **********(name varchar(20),age number,gender varchar(20),phno number(20),code_lang varchar(20),uname varchar(20),pass varchar(20))");
 		System.out.println("Tbale is created ");
 		con.close();
 	}
